093915f7fc0dde1b56303c995b9fc8cc7f085609
borzeman <38296338+borzeman@users.noreply.github.com>
2023-06-09 17:13:21 +0500

    Update settings.py v 0.0.3

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -16,16 +16,16 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework_simplejwt',
-    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api_yamdb',
+    'rest_framework_simplejwt',
+    'rest_framework',
     'django_filters',
+    'api_yamdb',
     'users',
     'api',
     'reviews',

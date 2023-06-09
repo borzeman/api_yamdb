d3607391f5875ddcf17ae4781e7c4e0a2009b41d
Lundovskikh Kirill <borzeman@gmail.com>
2023-06-09 14:42:41 +0500

    settings changes add

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangorestframework',
+    'djangorestframework-simplejwt',
     'api_yamdb',
     'django_filters',
     'users',
